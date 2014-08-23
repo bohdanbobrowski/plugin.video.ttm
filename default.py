4626ae1a2e9a9f500c83d296af0ec5221bb08c2f
Bohdan Bobrowski <bohdan@bobrowski.com.pl>
2014-08-23 21:18:06 +0200

    Poprawione regexy i (nieudana) próba dodania paginacji, lepszy cache itp.

--- default.py
+++ default.py
@@ -3,6 +3,7 @@ import hashlib
 import json
 import os
 import re
+import time
 import urllib
 import urllib2
 import xbmcaddon
@@ -17,14 +18,15 @@ addonID = addon.getAddonInfo('id')
 addonUserDataFolder = xbmc.translatePath("special://profile/addon_data/"+addonID)
 if not os.path.isdir(addonUserDataFolder):
     os.mkdir(addonUserDataFolder)
+linki = {}  
 
 def ListaKategorii():
         req = urllib2.Request('http://www.telewizjattm.pl/nasze-programy.html')
         req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
         response = urllib2.urlopen(req)
-        link=response.read()
+        html=response.read()
         response.close()
-        match=re.compile('<a href="([^"]*)" class="program [a-z\s]*" title="([^"]*)" ><img src="([^"]*)" alt="[^"]*" /></a>').findall(link)
+        match=re.compile('<a href="([^"]*)" class="program [a-z\s]*" title="([^"]*)" ><img src="([^"]*)" alt="[^"]*" /></a>').findall(html)
         categories = []
         for href,title,src in match:
             category = [title,href,src]
@@ -35,42 +37,75 @@ def ListaKategorii():
         categories_file.write(categories_json)
         categories_file.close()
 
-def ListaFilmow(url,name):
+def ListaFilmow(url,name,page):
+        url = url.replace('?play=on','')
+        # url = url.replace('.html','/2-strona.html')        
+        url_to_page = re.compile('(nasze-programy/[0-9]{1,3}-[a-z\-]*)').findall(url)[0]
+    
         url = 'http://www.telewizjattm.pl/' + url
-        req = urllib2.Request(url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link = response.read()
-        response.close()
-        miniatura = re.compile('image: "([^"]*)",').findall(link)
-        plikwideo = re.compile('file:"(http\:\/\/www\.telewizjattm\.pl\/ttm_filmy\/_new\/[^"]*[\.flv|.mp4])"').findall(link)
-        tytul = re.compile('<span class="date"[\s]*>([^<^>]*)<\/span>[\s]*<h2 class="title">([^<^>]*)<\/h2>').findall(link)
-        if miniatura and miniatura[0] and plikwideo and plikwideo[0] and tytul and tytul[0]:
-            addLink(tytul[0][0] + " - " + tytul[0][1], plikwideo[0], 'http://www.telewizjattm.pl/' + miniatura[0])            
-        linki = re.compile('</li>[\s]*<li><a href="([^"]*)" title').findall(link)
-        videos = {}
+        print "Url: "+url
+
+        m = hashlib.md5()
+        m.update(url+time.strftime("%Y%m%d"))
+        cache_hash = m.hexdigest()
+
+        print "Url: "+str(url)
+        print "Name: "+str(name)
+        print "Strona: "+str(page)        
+        if os.path.isfile(addonUserDataFolder + "/"+cache_hash+".json"):
+            # Wczytaj cache, o ile istnieje:
+            cache = {}
+            with open(addonUserDataFolder + "/"+cache_hash+".json", "r") as f:
+                for line in f:
+                    line = str(line)
+                    cache.update(json.loads(line))
+            cache_l = cache['linki']
+            cache_p = cache['podstrony']
+            linki = []
+            for l in cache_l:
+                linki = linki + [str(l)]
+            podstrony = []
+            for p in cache_p:
+                podstrony = podstrony + [str(p)]
+        else:
+            req = urllib2.Request(url)
+            req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+            response = urllib2.urlopen(req)
+            html = response.read()
+            response.close()
+            linki = [] + re.findall('<li>[^>^<]*<a href="(nasze-programy[^"]*)" title="[^"]*"',html)
+            podstrony = re.findall('<a href="('+url_to_page+'[^"]*)" class="page">[0-9]*</a>',html)
+            # Zapisz cache:
+            cache = {} 
+            cache['linki'] = linki
+            cache['podstrony'] = podstrony
+            cache_json = json.dumps(cache)
+            cache_file = open(addonUserDataFolder + "/"+cache_hash+".json", "w")
+            cache_file.write(cache_json)
+            cache_file.close()        
         # Sprawdzamy czy plik podręcznej pamięci już istnieje - jeżeli tak wczytujemy go, by przyspieszyć odczywalnie działanie wtyczki
+        videos = {}
         if os.path.isfile(addonUserDataFolder + "/videos.json"):
             with open(addonUserDataFolder + "/videos.json", "r") as f:
                 for line in f:
-                    videos.update(json.loads(line))
+                    videos.update(json.loads(line))            
         for h in linki:
-            url = 'http://www.telewizjattm.pl/' + h
+            video_url = 'http://www.telewizjattm.pl/' + h
             m = hashlib.md5()
-            m.update(url)
+            m.update(video_url)
             url_hash = m.hexdigest()
             if videos.get(url_hash) is not None:
                 if videos[url_hash]:
                     addLink(videos[url_hash]['title'], videos[url_hash]['file'], 'http://www.telewizjattm.pl/' + videos[url_hash]['thumb']) 
-            if videos.get(url_hash) is None:
-                req = urllib2.Request(url)            
-                req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-                response = urllib2.urlopen(req)
-                link = response.read()
-                response.close()
-                miniatura = re.compile('image: "([^"]*)",').findall(link)
-                plikwideo = re.compile('file:"(http\:\/\/www\.telewizjattm\.pl\/ttm_filmy\/_new\/[^"]*[\.flv|.mp4])"').findall(link)
-                tytul = re.compile('<span class="date"[\s]*>([^<^>]*)<\/span>[\s]*<h2 class="title">([^<^>]*)<\/h2>').findall(link)
+            else:
+                req_v = urllib2.Request(video_url)            
+                req_v.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+                response_v = urllib2.urlopen(req_v)
+                html_v = response_v.read()
+                response_v.close()
+                miniatura = re.compile('image: "([^"]*)",').findall(html_v)
+                plikwideo = re.compile('file:"(http\:\/\/www\.telewizjattm\.pl\/ttm_filmy\/_new\/[^"]*[\.flv|.mp4])"').findall(html_v)
+                tytul = re.compile('<span class="date"[\s]*>([^<^>]*)<\/span>[\s]*<h2 class="title">([^<^>]*)<\/h2>').findall(html_v)
                 if miniatura and miniatura[0] and plikwideo and plikwideo[0] and tytul and tytul[0]:
                     videos[url_hash] = {}
                     videos[url_hash]['title'] = tytul[0][0] + " - " + tytul[0][1]
@@ -79,11 +114,19 @@ def ListaFilmow(url,name):
                     addLink(tytul[0][0] + " - " + tytul[0][1], plikwideo[0], 'http://www.telewizjattm.pl/' + miniatura[0]) 
                 else:
                     videos[url_hash] = False
+        if len(podstrony) >= page:
+            next_page_url = url_to_page+'/'+str(page+1)+'-strona.html'
+            if name.find(' - Strona') > -1:
+                name = name.split(' - Strona');
+                name = name[0]
+            addPageLink(name+" - Strona "+str(page+1), next_page_url, page+1)
+
+        # Cache z informacjami o plikach wideo:
         videos_json = json.dumps(videos)
         videos_file = open(addonUserDataFolder + "/videos.json", "w")
         videos_file.write(videos_json)
-        videos_file.close()
-                
+        videos_file.close()        
+
 def get_params():
         param=[]
         paramstring=sys.argv[2]
@@ -102,15 +145,22 @@ def get_params():
         return param
 
 def addLink(name,url,iconimage):
-        ok=True
-        liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
+        ok = True
+        liz = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
         liz.setInfo( type="Video", infoLabels={ "Title": name } )
-        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
+        ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
         return ok
 
+def addPageLink(name,url,page):
+        u = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+'&page='+str(page)
+        ok = True
+        liz = xbmcgui.ListItem(name, iconImage="DefaultVideo.png")
+        liz.setInfo( type="Video", infoLabels={ "Title": name })
+        ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        return ok
 
 def addDir(name,url,iconimage,page):
-        u = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+        u = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+'&page='+str(page)
         ok = True
         liz = xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
         liz.setInfo( type="Video", infoLabels={ "Title": name } )
@@ -134,17 +184,15 @@ try:
         mode=int(params["mode"])
 except:
         pass
-
-print "Mode: "+str(mode)
-print "URL: "+str(url)
-print "Name: "+str(name)
+try:
+        page=int(params["page"])
+except:
+        pass
 
 if url==None or len(url)<1:
-        print ""
         ListaKategorii()
        
 else:
-        print ""+url
-        ListaFilmow(url,name)
+        ListaFilmow(url,name,page)
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
